95b5c4cbe7f23f9baff6574ccf990f1f602e3faf
Juan Cruz Castro Lopez <juan.castrolopez@outlook.com>
2018-09-20 13:45:03 -0300

    Agrego paquetes src y test

---
+++ src/meli/mutantdetector/MutantDetector.java
@@ -0,0 +1,11 @@
+/* AMCO @ 2018 */
+
+package meli.mutantdetector;
+
+public class MutantDetector {
+
+    public boolean isMutant(final String[] dna) {
+        
+    }
+
+}
---
+++ src/meli/mutantdetector/nitrogenbase/NitrogenBase.java
@@ -0,0 +1,8 @@
+/* AMCO @ 2018 */
+
+package meli.mutantdetector.nitrogenbase;
+
+public class NitrogenBase {
+    
+    public boolean analyzeDiagonalUpperLeft 
+}
